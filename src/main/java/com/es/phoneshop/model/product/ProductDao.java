b7e343201d01cc6b58b442f5650a76bcaf580312
Dmitry <20125dimanovogrodsky@gmail.com>
2022-01-25 18:56:12 +0300

    Revert "Task 1.5: Implement ProductDao and fixed images"
    This reverts commit 827b9e9c4c70ac92d434f97895e9f25a65d0699f.

--- src/main/java/com/es/phoneshop/model/product/ProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ProductDao.java
@@ -1,11 +1,10 @@
 package com.es.phoneshop.model.product;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public interface ProductDao {
-    Product getProduct(Long id) throws NoSuchElementException;
+    Product getProduct(Long id);
     List<Product> findProducts();
     void save(Product product);
-    void delete(Long id) throws NoSuchElementException;
+    void delete(Long id);
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,14 +3,10 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.math.BigDecimal;
-import java.util.Currency;
-import java.util.NoSuchElementException;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
-
-// just need to do some more tests
-public class ArrayListProductDaoTest {
+public class ArrayListProductDaoTest
+{
     private ProductDao productDao;
 
     @Before
@@ -20,30 +16,6 @@ public class ArrayListProductDaoTest {
 
     @Test
     public void testFindProductsNoResults() {
-        assertFalse(productDao.findProducts().isEmpty());
-    }
-
-    @Test
-    public void testSaveNewProduct(){
-        Currency usd = Currency.getInstance("USD");
-        Product product = new Product("test-product", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
-        productDao.save(product);
-        assertTrue(product.getId() > 0);
-        Product result = productDao.getProduct(Long.valueOf(product.getId()));
-        assertNotNull(result);
-        assertEquals("test-product", result.getCode());
-    }
-
-    @Test
-    public void testDeleteProduct() {
-        Currency usd = Currency.getInstance("USD");
-        Product product = new Product("test-product", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
-        productDao.save(product);
-        productDao.delete(product.getId());
-        try {
-            productDao.getProduct(product.getId());
-        } catch (NoSuchElementException e){
-            System.out.println(e);
-        }
+        assertTrue(productDao.findProducts().isEmpty());
     }
 }
