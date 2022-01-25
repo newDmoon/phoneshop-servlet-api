becfdc9f73dc701877b354826476578d6b929e82
Dmitry <20125dimanovogrodsky@gmail.com>
2022-01-25 16:21:11 +0300

    Task 1.6: Implement some tests and fixed images

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,10 +3,14 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.NoSuchElementException;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+
+// just need to do some more tests
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -16,6 +20,30 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testSaveNewProduct(){
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("test-product", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        assertTrue(product.getId() > 0);
+        Product result = productDao.getProduct(Long.valueOf(product.getId()));
+        assertNotNull(result);
+        assertEquals("test-product", result.getCode());
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("test-product", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        productDao.delete(product.getId());
+        try {
+            productDao.getProduct(product.getId());
+        } catch (NoSuchElementException e){
+            System.out.println(e);
+        }
     }
 }
